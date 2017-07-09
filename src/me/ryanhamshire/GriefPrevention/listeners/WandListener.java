e59ccfa1c520488a1e454f1073842e32ba891bef
RoboMWM <robomwm@users.noreply.github.com>
2017-07-09 09:20:28 -0700

    progress

---
+++ src/me/ryanhamshire/GriefPrevention/listeners/WandListener.java
@@ -0,0 +1,16 @@
+package me.ryanhamshire.GriefPrevention.listeners;
+
+import org.bukkit.event.Listener;
+
+/**
+ * Created on 5/28/2017.
+ *
+ * @author RoboMWM
+ */
+public class WandListener implements Listener
+{
+    void onWandEvent()
+    {
+
+    }
+}
