fa689162f64e0dd15a7047d3284458b784f0873b
RoboMWM <robomwm@users.noreply.github.com>
2020-05-25 10:28:32 -0700

    TIL about maven surefire and how I need to use an updated version for junit 5

--- src/test/java/me/ryanhamshire/GriefPrevention/TestPlayerCreateClaimWithAnnotations.java
+++ src/test/java/me/ryanhamshire/GriefPrevention/TestPlayerCreateClaimWithAnnotations.java
@@ -4,11 +4,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
@@ -19,17 +16,16 @@ import static org.mockito.Mockito.when;
  *
  * @author RoboMWM
  */
-@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
-@ExtendWith(MockitoExtension.class)
 public class TestPlayerCreateClaimWithAnnotations
 {
 
     Player player = mock(Player.class, RETURNS_DEEP_STUBS);
     PlayerInteractEvent event;
 
-    @BeforeAll
+    @BeforeEach
     public void holdTool()
     {
+        System.out.println("before");
         when(player.getInventory().getItemInMainHand().getType()).thenReturn(Material.GOLDEN_SHOVEL);
         when(player.getTargetBlock(null, 100).getLocation()).thenReturn(new Location(null, -100, 25, -100), new Location(null, 100, 95, 100));
     }
